6da827c360753d9ab05bdb9d6d2ee46e5192f6ae
Admin O <olexandro@gmail.com>
2022-10-16 23:30:59 +0300

    + проверка через пробегание по массиву чаров

--- src/Data.java
+++ src/Data.java
@@ -12,7 +12,7 @@ public class Data {
 
     private static boolean checkLogin(String login) throws WrongLoginException {
         boolean checkLog = false;
-        if (login.matches("^[a-z0-9_]{1,20}$")) {
+        if (login.matches("^[a-zA-Z0-9_]{1,20}$")) {
             checkLog = true;
         } else {
             throw new WrongLoginException("Неверно введен логин!");
@@ -22,11 +22,23 @@ public class Data {
 
     private static boolean checkPassword(String password, String confirmPassword) throws WrongPasswordException {
         boolean checkPass = false;
-        if (password.matches("^[a-z0-9_]{1,20}$") && confirmPassword.equals(password)) {
+        if (password.matches("^[a-zA-Z0-9_]{1,20}$") && confirmPassword.equals(password)) {
             checkPass = true;
         } else {
             throw new WrongPasswordException("Неверно введен пароль или пароли не совпадают!");
         }
         return checkPass;
     }
+
+//    static boolean checkCharacter(String checkString) {
+//        if (checkString.length() < 20 && checkString.length() > 1) {
+//            return false;
+//        }
+//        var array :char[] =checkString.toCharArray();
+//        for (char symbol : array) {
+//            if (!(Character.isAlphabetic(symbol) || Character.isDigit(symbol) || symbol == '_')) {
+//                return false
+//            }
+//        }
+//    }
 }
